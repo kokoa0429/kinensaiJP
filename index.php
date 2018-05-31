da5f7b4ca532cf97356e6d7d8d44899444123b37
kokoa0429 <kokoa@sweak.net>
2018-05-31 22:31:01 +0900

    Add template
    Add template

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+//kobato kawaii prpr
+?>
+<!DOCTYPE HTML>
+<!--
+	Identity by HTML5 UP
+	html5up.net | @ajlkn
+	edit by @love_kobato_all
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>130th 記念祭ページ</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-loading">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Main -->
+					<section id="main">
+						<header>
+							<span class="avatar"><img src="images/avatar.jpg" alt="" /></span>
+							<h1>130th 記念祭 2018/9/29.30</h1>
+							<p>現在ページ制作中です。しばらくお待ちください。</p>
+						</header>
+						<footer>
+							<ul class="icons">
+								<li><a href="https://twitter.com/kinensai" class="fa-twitter">Twitter</a></li>
+							</ul>
+						</footer>
+					</section>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<ul class="copyright">
+							<li>&copy; Jane Doe</li><li>Design: <a href="http://html5up.net">HTML5 UP</a> | edit : kinensaijp  sweak.net</li>
+						</ul>
+					</footer>
+
+			</div>
+
+			<script>
+				if ('addEventListener' in window) {
+					window.addEventListener('load', function() { document.body.className = document.body.className.replace(/\bis-loading\b/, ''); });
+					document.body.className += (navigator.userAgent.match(/(MSIE|rv:11\.0)/) ? ' is-ie' : '');
+				}
+			</script>
+
+	</body>
+</html>
